51868038332b728aef613bc6fcceb69982310de4
dimitri-hoareau-WEL <dimitri.hoareau@weuplearning.com>
2023-06-26 11:21:11 +0200

    update code to match with new json

--- src/thumbnailGallery/ThumbnailGallery.tsx
+++ src/thumbnailGallery/ThumbnailGallery.tsx
@@ -8,10 +8,8 @@ type Props = {
 };
 
 // Thumbnail gallery component that displays webinars
+
 const ThumbnailGallery: React.FC<Props> = ({ webinars }) => {
-
-
-  // Render the thumbnail gallery with the filtered webinars
   return (
     <div className="thumbnail-gallery">
       {webinars.map((webinar) => (
@@ -20,11 +18,20 @@ const ThumbnailGallery: React.FC<Props> = ({ webinars }) => {
           <div className="thumbnail__content">
             <h3 className="thumbnail__title">{webinar.title}</h3>
             <p className="thumbnail__date">{moment(webinar.date).format("dddd Do MMMM YYYY, h:mm a")}</p>
-            <p className="thumbnail__description">{webinar.description}</p>
+            {webinar.description && (
+              <p className="thumbnail__description">
+                {webinar.description.map((line, index) => (
+                  <React.Fragment key={index}>
+                    {line}
+                    <br />
+                  </React.Fragment>
+                ))}
+              </p>
+            )}
             <p className="thumbnail__speaker">{webinar.speaker.name}</p>
             <p className="thumbnail__speaker_position">{webinar.speaker.position}</p>
             <p className="thumbnail__tag">{webinar.tag}</p>
-            <button className="thumbnail__button" onClick={() => window.location.href = webinar.url_enroll}>
+            <button className="thumbnail__button" onClick={() => (window.location.href = webinar.url_enroll)}>
               Je m'inscris
             </button>
           </div>
--- src/type.ts
+++ src/type.ts
@@ -1,15 +1,14 @@
 // types.ts
 
 export type Webinar = {
-    title: string;
-    date: string;
-    description: string;
-    speaker: {
-      name: string;
-      position: string;
-    };
-    tag: string;
-    image: string;
-    url_enroll: string;
+  title: string;
+  date: string;
+  description: string[];
+  speaker: {
+    name: string;
+    position: string;
   };
-  
+  tag: string;
+  image: string;
+  url_enroll: string;
+};
